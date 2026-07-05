1ec2d6fe7b50576553dd1e1b51f1f84ffc5d512e
K.P.Shashin Peiris <shashinsandeepa2002@gmail.com>
2026-07-06 02:20:40 +0530

    updated

--- backend/ApparelHubERP.API/Controllers/AuthController.cs
+++ backend/ApparelHubERP.API/Controllers/AuthController.cs
@@ -6,14 +6,9 @@ namespace ApparelHubERP.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase
+public class AuthController(IAuthService authService) : ControllerBase
 {
-    private readonly IAuthService _authService;
-
-    public AuthController(IAuthService authService)
-    {
-        _authService = authService;
-    }
+    private readonly IAuthService _authService = authService;
 
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
